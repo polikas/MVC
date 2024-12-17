1c0f42a98e329acbbda5f105504f6ab5ff8f36ab
Stavros Charitos <polikasxaritos@gmail.com>
2024-12-17 09:49:17 +0000

    build relationships, one to one, one to many, mane to many

--- MyApp/Controllers/ItemsController.cs
+++ MyApp/Controllers/ItemsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MyApp.Data;
 using MyApp.Models;
@@ -15,16 +16,21 @@ namespace MyApp.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var item = await _context.Items.ToListAsync();
+            var item = await _context.Items.Include(s => s.SerialNumber)
+                                            .Include(c => c.Category)
+                                            .Include(ic => ic.ItemClients)
+                                            .ThenInclude(c => c.Client)
+                                            .ToListAsync();
             return View(item);
         }
 
         public IActionResult Create()
         {
+            ViewData["Categories"] = new SelectList(_context.Categories, "Id", "Name");
             return View();
         }
         [HttpPost]
-        public async Task<IActionResult> Create([Bind("Id, Name, Price")] Item item)
+        public async Task<IActionResult> Create([Bind("Id, Name, Price, CategoryId")] Item item)
         {
             if(ModelState.IsValid)
             {
@@ -37,11 +43,12 @@ namespace MyApp.Controllers
 
         public async Task<IActionResult> Edit(int id)
         {
+            ViewData["Categories"] = new SelectList(_context.Categories, "Id", "Name");
             var item = await _context.Items.FirstOrDefaultAsync(x => x.Id == id);
             return View(item);
         }
         [HttpPost]
-        public async Task<IActionResult> Edit(int id, [Bind("Id, Name, Price")] Item item)
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Name, Price, CategoryId")] Item item)
         {
             if(ModelState.IsValid)
             {
--- MyApp/Data/MyAppContext.cs
+++ MyApp/Data/MyAppContext.cs
@@ -7,6 +7,33 @@ namespace MyApp.Data
     {
         public MyAppContext(DbContextOptions<MyAppContext> options) : base(options) { }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ItemClient>().HasKey(ic => new
+            {
+                ic.ItemId,
+                ic.ClientId
+            });
+
+            modelBuilder.Entity<ItemClient>().HasOne(i => i.Item).WithMany(ic => ic.ItemClients).HasForeignKey(i => i.ItemId);
+            modelBuilder.Entity<ItemClient>().HasOne(c => c.Client).WithMany(ic => ic.ItemClients).HasForeignKey(c => c.ClientId);
+
+            modelBuilder.Entity<Item>().HasData(
+                new Item { Id = 4, Name="microphone", Price=40,SerialNumberId=10});
+            modelBuilder.Entity<SerialNumber>().HasData(
+                new SerialNumber { Id = 10, Name = "MIC150", ItemId=4 });
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Electronics" },
+                new Category { Id = 2, Name = "Books" }
+                );
+        }
+
         public DbSet<Item> Items { get; set; }
+        public DbSet<SerialNumber> SerialNumbers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<ItemClient> ItemClients { get; set; }
     }
 }
